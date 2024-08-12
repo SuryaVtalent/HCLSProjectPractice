cac924200b0f4ba106b4c448fb02c5beff6f6ad1
SuryaVtalent <suryaa.vtalent@gmail.com>
2024-08-12 13:56:42 +0530

    This is my DemoProject Using Arithmetic Operations

---
+++ DemoProject/DemoProjectSol/DemoProjectEx/Operations/ArithmeticOpt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectEx.Operations
+{
+    public class ArithmeticOpt
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+
+        public int Sub(int x,int y)
+        {
+            return x - y;
+        }
+        public int Mul(int x,int y)
+        {
+            return x*y;
+        }
+        public int Div(int x,int y)
+        {
+            return x / y;
+        }
+        public int Rem(int x,int y)
+        {
+            return x % y;
+        }
+        public int Sum(int x,int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ DemoProject/DemoProjectSol/DemoProjectEx/Program.cs
@@ -0,0 +1,31 @@
+﻿using DemoProjectEx.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = 0;
+            ArithmeticOpt obj = new ArithmeticOpt();
+            res=obj.Add(10,20);
+            Console.WriteLine("The sum is "+res);
+            res = obj.Sub(20, 10);
+            Console.WriteLine("The Sub is " + res); 
+            res = obj.Mul(10,20);
+            Console.WriteLine("Th Mul is " + res);
+            res = obj.Div(10, 20);
+            Console.WriteLine("Th Div is " + res);
+            res = obj.Rem(10, 20);
+            Console.WriteLine("Th Rem is " + res);
+            res = obj.Sum(10, 20);
+            Console.WriteLine("Th Rem is " + res);
+            Console.ReadKey();
+        }
+    }
+}
